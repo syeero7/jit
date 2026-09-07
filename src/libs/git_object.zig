c145611ec7d0f8312bbc3c4a528056578a2e97a7
emal <emalhasitha@proton.me>
2026-09-07 00:17:41 +0000

    feat: add functionality to write git objects

--- src/libs/git_object.zig
+++ src/libs/git_object.zig
@@ -23,19 +23,61 @@ const GitObjKind = enum {
     Tree,
     Blob,
     Tag,
+
+    pub fn toString(self: GitObjKind) []const u8 {
+        switch (self) {
+            .Commit => return []const u8{"commit"},
+            .Tree => return []const u8{"tree"},
+            .Blob => return []const u8{"blob"},
+            .Tag => return []const u8{"tag"},
+        }
+    }
+};
+
+const GitObjData = union(GitObjKind) {
+    Commit: []const u8,
+    Tree: []const u8,
+    Blob: []const u8,
+    Tag: []const u8,
 };
 
 pub const GitObject = struct {
     kind: GitObjKind = undefined,
-    data: []const u8 = undefined,
+    data: GitObjData = undefined,
 
     pub fn init(kind: GitObjKind, data: []const u8) GitObject {
-        return .{ .kind = kind, .data = data };
+        return .{ .kind = kind }.parse(data);
     }
 
-    pub fn parse() !void {}
+    pub fn parse(self: GitObject, data: []const u8) GitObject {
+        switch (self.kind) {
+            .Commit => {
+                self.data.Commit = data;
+                return self;
+            },
+            .Tree => {
+                self.data.Tree = data;
+                return self;
+            },
+            .Blob => {
+                self.data.Blob = data;
+                return self;
+            },
+            .Tag => {
+                self.data.Tag = data;
+                return self;
+            },
+        }
+    }
 
-    pub fn serialize() !void {}
+    pub fn serialize(self: GitObject) ![]const u8 {
+        switch (self.kind) {
+            .Commit => return self.data.Commit,
+            .Tree => return self.data.Tree,
+            .Blob => return self.data.Blob,
+            .Tag => return self.data.Tag,
+        }
+    }
 };
 
 pub fn read(allocator: Allocator, io: Io, repo: Repository, hash: []const u8) !GitObject {
@@ -78,7 +120,43 @@ pub fn read(allocator: Allocator, io: Io, repo: Repository, hash: []const u8) !G
     return Error.MalformedObject;
 }
 
-pub fn write() !void {}
+pub fn write(alloc: Allocator, io: Io, obj: GitObject, repo: ?Repository) ![]const u8 {
+    const data = try obj.serialize();
+    const length = try std.fmt.allocPrint(alloc, "{d}", .{data.len});
+    const parts = [_][]const u8{ obj.kind.toString(), " ", length, '\x00', data };
+    const result = try std.mem.concat(alloc, u8, &parts);
+
+    var digest: [Sha1.digest_length]u8 = undefined;
+    var hasher = Sha1.init(.{});
+    var hex_buf: [(160 / 8) * 2]u8 = undefined;
+    hasher.update(result);
+    hasher.final(&digest);
+
+    const hash = try std.fmt.bufPrint(&hex_buf, "{x}", .{digest});
+    if (repo) |r| {
+        const path = [_][]const u8{ r.gitdir, "objects", hash[0..2] };
+        const dir_path = try std.fs.path.join(alloc, &path);
+        const cwd = Io.Dir.cwd();
+        try cwd.createDirPath(io, dir_path);
+
+        const obj_path = try std.fs.path.join(alloc, &[_][]const u8{ dir_path, hash[2..] });
+        const file = try cwd.createFile(io, obj_path, .{});
+        defer file.close(io);
+
+        var buffer: [1024 * 4]u8 = undefined;
+        var file_writer = file.writer(io, &buffer);
+        const writer = &file_writer.interface;
+
+        var comp_buf: [flate.max_window_len]u8 = undefined;
+        var compressor = try flate.Compress.init(writer, &comp_buf, .zlib, .{});
+        const comp_writer = &compressor.writer;
+
+        try comp_writer.write(result);
+        try comp_writer.flush();
+    }
+
+    return hash;
+}
 
 test "read hash" {
     var arena = std.heap.ArenaAllocator.init(testing.allocator);
